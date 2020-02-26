d2ccf6b1ff2d72594c82433f57c90f96b774f52d
Alexei Dodon <adodon@luxoft.com>
2020-02-26 08:31:10 -0800

    NSM patch updated after changes on master branch in NSM code

--- kind/nsm_install.sh
+++ kind/nsm_install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## This script installs NSM inside Kind (Kubernetes in Docker)
+## This script installs NSM inside KinD (Kubernetes in Docker)
 
 # Make sure minikube is not running
 minikube stop > /dev/null 2>&1
@@ -18,12 +18,21 @@ else
     cd $WORKINGDIR
 fi
 
+# Check if KinD is installed
+kind version > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo "KinD is installed"
+else
+    echo "KinD is not installed"
+    GO111MODULE="on" go get sigs.k8s.io/kind@v0.7.0
+fi
+
 # Check if NSM is installed
 kubectl get pods -A | grep nsmgr > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "NSM is installed with Kind"
+    echo "NSM is installed with KinD"
 else
-    echo "NSM is not installed with Kind"
+    echo "NSM is not installed with KinD"
     if [ -d "networkservicemesh" ]; then
         echo "networkservicemesh directory already exists"
         cd networkservicemesh
@@ -31,7 +40,17 @@ else
         git clone https://github.com/networkservicemesh/networkservicemesh.git
         cp ../nsm_patch.diff networkservicemesh/
         cp testing_patch.diff networkservicemesh/
-        cd networkservicemesh && git apply nsm_patch.diff && git apply testing_patch.diff
+        cd networkservicemesh
+        git apply nsm_patch.diff
+        if [ $? -ne 0 ]; then
+            echo "Applying an NSM patch failed"
+            exit 1
+        fi
+        git apply testing_patch.diff
+        if [ $? -ne 0 ]; then
+            echo "Applying an NSM patch (for testing) failed"
+            exit 1
+        fi
     fi
 
     make k8s-build
@@ -40,7 +59,7 @@ else
 
     TILLER_IMAGE=gcr.io/kubernetes-helm/tiller:v2.16.1
     docker pull $TILLER_IMAGE
-    # load Tiller image into kind
+    # load Tiller image into KinD
     kind load docker-image $TILLER_IMAGE --name nsm
 
     make k8s-load-images
