64cbf8f9338dad64eb59a8cb5827859d90c4d440
Alexei Dodon <adodon@cisco.com>
2020-11-27 10:11:58 +0200

    updated wcmctl script

--- utils/wcm_wcmctl_deployment.sh
+++ utils/wcm_wcmctl_deployment.sh
@@ -13,20 +13,20 @@ cat system_topo/config/kind_clustermaps.sh | sed 's@=\${KCONFDIR_ROOT}@=/go/src/
 kind create cluster --name kind1
 kind get kubeconfig --name=kind1 > ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig
 sleep 20
-wcmctl install central-cluster-components -k ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig
+GOPATH=/go wcmctl install central-cluster-components -k ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig
 
 kind create cluster --name kind2
 kind get kubeconfig --name=kind2 > ${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-2.kubeconfig
 sleep 20
-wcmctl install member-cluster-components --central-kubeconfig ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig --prefix 254 -k ${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-2.kubeconfig
+GOPATH=/go wcmctl install member-cluster-components --central-kubeconfig ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig --prefix 254 -k ${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-2.kubeconfig
 
 kind create cluster --name kind3
 kind get kubeconfig --name=kind3 > ${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-3.kubeconfig
 sleep 20
-wcmctl install member-cluster-components --central-kubeconfig ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig --prefix 253 -k ${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-3.kubeconfig
+GOPATH=/go wcmctl install member-cluster-components --central-kubeconfig ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig --prefix 253 -k ${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-3.kubeconfig
 
 sleep 10
-wcmctl create connectivitydomain -k ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig --name example --memberConfig=${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-2.kubeconfig,${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-3.kubeconfig
+GOPATH=/go wcmctl create connectivitydomain -k ${WCM_SYSTEM_DIR}/kubeconfigs/central/kind-1.kubeconfig --name example --memberConfig=${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-2.kubeconfig,${WCM_SYSTEM_DIR}/kubeconfigs/nsm/kind-3.kubeconfig
 
 ${WCM_SYSTEM_DIR}/system_topo/deploy_demo_app.sh --cluster-map-file=${WCM_SYSTEM_DIR}/system_topo/config/systest_clustermap.sh --component-map-file=${WCM_SYSTEM_DIR}/system_topo/config/wcm-3-cluster.sh --service-name=example --nsc-delay=10
 
